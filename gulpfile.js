15694f56c3fafd5567a908fb831acd1e2d2c5428
Nathan Demick <ndemick@gmail.com>
2014-11-04 22:43:15 -0500

    Update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	browserify = require('gulp-browserify'),
+	rename = require('gulp-rename'),
+	less = require('gulp-less'),
+	watch = require('gulp-watch');
 
 gulp.task('default', function () {
 	console.log('do nothing');
+});
+
+gulp.task('coffee', function () {
+	gulp.src('src/app.coffee', { read: false })
+		.pipe(browserify({
+			transform: ['coffeeify', 'jstify'],
+			extensions: ['.coffee', '.html']
+		}))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', function () {
+	gulp.src('dist/assets/stylesheets/main.less')
+		.pipe(less())
+		.pipe(gulp.dest('dist/assets/stylesheets'));
+})
+
+gulp.task('watch', function () {
+	gulp.watch('**/*.coffee', function (files, callback) {
+		gulp.start('coffee', callback);
+	});
 });
