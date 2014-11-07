0c2d6479941406d25f668f470e6001a33583bf45
Nathan Demick <ndemick@gmail.com>
2014-11-07 06:36:49 -0500

    Try adding "watch" task

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,6 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	watch = require('gulp-watch');
 
-gulp.task('default', function () {
-	console.log('do nothing');
-});
-
 gulp.task('coffee', function () {
 	gulp.src('src/app.coffee', { read: false })
 		.pipe(browserify({
@@ -22,10 +18,12 @@ gulp.task('less', function () {
 	gulp.src('src/stylesheets/main.less')
 		.pipe(less())
 		.pipe(gulp.dest('dist/assets/stylesheets'));
-})
+});
+
+gulp.task('build', function () {
+	gulp.start(['coffee', 'less']);
+});
 
 gulp.task('watch', function () {
-	gulp.watch('**/*.coffee', function (files, callback) {
-		gulp.start('coffee', callback);
-	});
+	gulp.watch(['src/**/*.coffee'], ['coffee']);
 });
